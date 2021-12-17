a693bbd60a02c7ad99947930f66502a078a51367
Alessandro Cerea <alessandro.cerea@tridentch.ch>
2021-12-17 17:45:05 +0100

    logo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,7 +22,20 @@
     </head>
     <body class="antialiased">
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-            <img src="url('/storage/img/logo-weroad-bianco.jpg') }}" alt="Main LOGO">    
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                    @endauth
+                        <a href="{{ route('travels.all') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Travels</a>
+                </div>
+            @endif
+
+            <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+            <img src="storage/img/logo-weroad-bianco.jpg" alt="Main LOGO">    
+        </div>
         </div>
     </body>
 </html>
